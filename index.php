b10c9c39c2a92129cb7896d3d99ea0c694a0d7ce
Pritam Anand <pritamanand60@gmail.com>
2026-07-21 10:23:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niles &amp; Sinai || Luxury Fine Jewellery</title>
+    <link rel="stylesheet" href="Assets/Modules/base.css">
+</head>
+    <body>
+        <?php 
+              $module = 'Assets/Modules/';
+              $landing = 'Assets/Website/Contents/Landing/';
+              include $module  . 'nav.php';
+              include $landing . 'LC-HS-001.php'; 
+              include $landing . 'LC-SS-002.php'; 
+              include $landing . 'LC-PS-003.php'; 
+              include $landing . 'LC-CS-004.php'; 
+              include $landing . 'LC-FQ-005.php'; 
+              include $landing . 'LC-VR-006.php'; 
+              include $landing . 'LC-VS-007.php'; 
+              include $landing . 'LC-CU-009.php'; 
+              include $module  . 'footer.php';
+         ?>
+    </body>
+</html>
